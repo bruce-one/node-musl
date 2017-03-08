9ff0febf08cf15ea4f2ab6b7bd9b155519ae0c72
Bryce Gibson <bryce@gibson-consulting.com.au>
2017-03-09 07:35:26 +1100

    Use exec rather than spawn to get better errors.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 const { join } = require('path')
-const { spawnSync } = require('child_process')
+const { execFileSync } = require('child_process')
 const packageJson = require('./package.json')
 
 module.exports.wrap = wrap
 function wrap(cmd) {
   return function wrapSync(args, opts = { stdio: 'inherit' }) {
-    return spawnSync(join(__dirname, packageJson.bin[`musl-${cmd}`]), args, opts)
+    return execFileSync(join(__dirname, packageJson.bin[`musl-${cmd}`]), args, opts)
   }
 }
 
@@ -16,5 +16,5 @@ const gccWrapSync = module.exports.gccWrapSync = wrap('gcc')
 module.exports.gxxWrapSync = wrap('g++')
 
 if(require.main === module) {
-  process.exitCode = gccWrapSync(process.argv.slice(2)).status
+  gccWrapSync(process.argv.slice(2))
 }
