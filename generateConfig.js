cc208971f6e609446c8d86635247d6e971ec01c5
Bryce Gibson <bryce@gibson-consulting.com.au>
2017-03-09 09:17:22 +1100

    Actually use bootstrap.

--- generateConfig.js
+++ generateConfig.js
@@ -10,7 +10,7 @@ TARGET=${target}
 OUTPUT=${path.resolve(__dirname, `${process.arch}-linux-musl`)}
 DL_CMD=${path.resolve(__dirname, 'download.js')}
 
-# COMMON_CONFIG += CC="${path.resolve(__dirname, 'bootstrap/bin/${muslArch}-linux-musl-gcc')}" CXX="${path.resolve(__dirname, 'bootstrap/bin/${muslArch}-linux-musl-g++')}" LD="${path.resolve(__dirname, 'bootstrap/bin/${muslArch}-linux-musl-ld')}"
+COMMON_CONFIG += CC="${path.resolve(__dirname, 'bootstrap/bin/${muslArch}-linux-musl-gcc')}" CXX="${path.resolve(__dirname, 'bootstrap/bin/${muslArch}-linux-musl-g++')}" LD="${path.resolve(__dirname, 'bootstrap/bin/${muslArch}-linux-musl-ld')}"
 COMMON_CONFIG += CFLAGS="-g0 -Os -static --static" CXXFLAGS="-g0 -Os -static --static" LDFLAGS="-s -static"
 COMMON_CONFIG += --disable-nls
 GCC_CONFIG += --enable-languages=c,c++
