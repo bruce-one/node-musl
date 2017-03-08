33c692930cc1822cb68f0489859f56ec11bc3bee
Bryce Gibson <bryce@gibson-consulting.com.au>
2017-03-08 13:19:29 +1100

    Remove verbose flag from gcc build flags.

--- generateConfig.js
+++ generateConfig.js
@@ -8,7 +8,7 @@ OUTPUT=${path.resolve(__dirname, target)}
 DL_CMD=${path.resolve(__dirname, 'download.js')}
 
 # COMMON_CONFIG += CC="${path.resolve(__dirname, 'bootstrap/bin/x86_64-linux-musl-gcc')}" CXX="${path.resolve(__dirname, 'bootstrap/bin/x86_64-linux-musl-g++')}" LD="${path.resolve(__dirname, 'bootstrap/bin/x86_64-linux-musl-ld')}"
-COMMON_CONFIG += CFLAGS="-v -g0 -Os -static --static" CXXFLAGS="-v -g0 -Os -static --static" LDFLAGS="-s -static"
+COMMON_CONFIG += CFLAGS="-g0 -Os -static --static" CXXFLAGS="-g0 -Os -static --static" LDFLAGS="-s -static"
 COMMON_CONFIG += --disable-nls
 GCC_CONFIG += --enable-languages=c,c++
 GCC_CONFIG += --disable-libquadmath --disable-decimal-float
