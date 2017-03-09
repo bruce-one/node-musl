bc3514f55ce286f115675ae6d66857e001131bc8
Bryce Gibson <bryce@gibson-consulting.com.au>
2017-03-09 11:30:30 +1100

    [publish] WIP fix symlinks.

--- symlinks.js
+++ symlinks.js
@@ -6,9 +6,13 @@ const fs = require('fs')
 const binary = require('node-pre-gyp')
 const binding_path = path.resolve(binary.find(path.resolve(path.join(__dirname,'./package.json'))), '..')
 
+
+const arch = process.argv[2] || path.basename(binding_path).split('-')[0]
+const muslArch = arch === 'x64' ? 'x86_64' : (arch === 'ia32' ? 'i686' : arch)
+
 Array('gcc', 'g++', 'ld').forEach( (bin) => {
   const binPath = path.join(__dirname, 'bin', `musl-${bin}`)
-  const target = path.relative(path.join(__dirname, 'bin'), path.join(binding_path, 'bin', `x86_64-linux-musl-${bin}`))
+  const target = path.relative(path.join(__dirname, 'bin'), path.join(binding_path, 'bin', `${muslArch}-linux-musl-${bin}`))
   fs.unlink(binPath, () => {
     fs.symlink(target, binPath, (err) => {
       if(err) throw err
